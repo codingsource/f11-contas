6b184b9bb15023ed303b96beda91bc7a6ef87771
Peter Thomas <peterpetrick@gmail.com>
2017-04-27 02:52:42 -0300

    Reduzindo metodo compare

--- src/br/com/caelum/contas/main/ComparadorPorTamanho.java
+++ src/br/com/caelum/contas/main/ComparadorPorTamanho.java
@@ -6,11 +6,7 @@ public class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
-		if (o1.length() < o2.length())
-			return -1;
-		if	(o2.length() < o1.length())
-			return 1;
-		return 0;
+		return Integer.compare(o1.length(), o2.length());
 	}
 	
 }
